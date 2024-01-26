df1b36a34ab5402bf8d5165a6a1897baf02404ab
avilwock <151412286+avilwock@users.noreply.github.com>
2024-01-26 15:44:37 -0800

    updated HTML time blocks to match demos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeElement = document.getElementById('currentDay');
+
+function updateTime() {
+  var currentDate = dayjs().format("MMM DD, YYYY [at] hh:mm:ss");
+  timeElement.textContent = currentDate;
+}
+setInterval(updateTime, 1000);
+
 $(document).ready(function() {
 });
   // TODO: Add a listener for click events on the save button. This code should
