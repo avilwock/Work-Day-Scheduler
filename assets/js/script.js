de1b40afc4a19351240dfab13db066d0338c3b39
avilwock <151412286+avilwock@users.noreply.github.com>
2024-01-27 15:20:15 -0800

    added if statement to JS, updated ids in html, save button and local storage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,61 @@ function updateTime() {
 setInterval(updateTime, 1000);
 
 $(document).ready(function() {
-});
+
+    $(".saveBtn").on("click", function() {
+    // Get nearby values of the description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+    
+      var timeNow = dayjs().hour();
+      console.log(timeNow);
+        $(".time-block").each(function() {
+          var timeBlock = parseInt($(this).attr("id"));
+          console.log(timeBlock);
+
+            if (timeBlock > timeNow) {
+              $(this).addClass("past");
+              $(this).removeClass("present");
+              $(this).removeClass("future");
+            }
+
+            else if (timeBlock === timeNow) {
+              $(this).removeClass("past");
+              $(this).addClass("present");
+              $(this).removeClass("future");
+                           
+            }
+            else (timeBlock < timeNow); {
+              $(this).removeClass("past");
+              $(this).removeClass("present");
+              $(this).addClass("future");              
+            }
+        })
+    }
+    timeTracker();
+
+    function getFromLocalStorage() {
+      $("#9 .description").val(localStorage.getItem("9"));
+      $("#10 .description").val(localStorage.getItem("10"));
+      $("#11 .description").val(localStorage.getItem("11"));
+      $("#12 .description").val(localStorage.getItem("12"));
+      $("#13 .description").val(localStorage.getItem("13"));
+      $("#14 .description").val(localStorage.getItem("14"));
+      $("#15 .description").val(localStorage.getItem("15"));
+      $("#16 .description").val(localStorage.getItem("16"));
+      $("#17 .description").val(localStorage.getItem("17"));
+            // localStorage.getItem(time, text)
+    }
+    getFromLocalStorage();
+})
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
