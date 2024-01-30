a7f7a55f1e912b1824572ab34cd5b25a0c54c3a1
avilwock <151412286+avilwock@users.noreply.github.com>
2024-01-29 17:36:34 -0800

    fixed variable names in HTML and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
       var timeNow = parseInt(dayjs().hour());
       
         $(".time-block").each(function() {
-          var timeBlock = parseInt($(this).attr("id"));
+          var timeBlock = parseInt($(this).attr('id').split('-')[1]);
           console.log(timeBlock, timeNow);
 
             if (timeBlock < timeNow) {
@@ -54,15 +54,16 @@ $(document).ready(function() {
     timeCheck();
 
     function getFromLocalStorage() {
-      $("#9 .description").val(localStorage.getItem("9"));
-      $("#10 .description").val(localStorage.getItem("10"));
-      $("#11 .description").val(localStorage.getItem("11"));
-      $("#12 .description").val(localStorage.getItem("12"));
-      $("#13 .description").val(localStorage.getItem("13"));
-      $("#14 .description").val(localStorage.getItem("14"));
-      $("#15 .description").val(localStorage.getItem("15"));
-      $("#16 .description").val(localStorage.getItem("16"));
-      $("#17 .description").val(localStorage.getItem("17"));
+      // $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+      // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+      // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+      // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+      // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+      // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+      // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+      // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+      // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+      for (let i = 9; i <= 17; i++) $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
     }
     getFromLocalStorage();
 
